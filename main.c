88ce1c43a585b6e8c977ca31486edf9ef496a2f0
lnarvaez <lnarvaez@student.42madrid.com>
2023-11-29 13:37:24 +0100

    texture

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include <mlx.h>
+#include <stdio.h>
 
 #define MAP_WIDTH 7
 #define MAP_HEIGHT 7
@@ -17,6 +18,12 @@
 typedef struct {
     char map[MAP_HEIGHT][MAP_WIDTH];
 } Map;
+
+typedef struct {
+    void *img;
+    int width;
+    int height;
+} Texture;
 
 void load_map(Map *map) {
     // Definir manualmente el mapa o leerlo desde un archivo de texto
@@ -49,32 +56,42 @@ void mlx_rectangle(void *mlx, void *win, int x, int y, int width, int height, in
     }
 }
 
-void draw_map(void *mlx, void *win, Map *map) {
+void draw_map(void *mlx, void *win, Map *map, Texture *texture) {
     int block_size = 30; // Tamaño de cada bloque en píxeles
-
     for (int i = 0; i < MAP_HEIGHT; i++) {
         for (int j = 0; j < MAP_WIDTH; j++) {
             int x = j * block_size;
             int y = i * block_size;
 
-           if (map->map[i][j] == '1') {
-                // Dibujar pared (color rojo)
-                mlx_rectangle(mlx, win, x, y, block_size, block_size, 0xFF0000);
+            if (map->map[i][j] == '1') {
+                // Dibujar pared con textura
+                mlx_put_image_to_window(mlx, win, texture->img, x, y);
             } else if (map->map[i][j] == 'P') {
-                // Dibujar posición inicial (color verde)
-                mlx_rectangle(mlx, win, x, y, block_size, block_size, 0x00FF00);
+                // Dibujar posición inicial con textura
+                mlx_put_image_to_window(mlx, win, texture->img, x, y);
             } else if (map->map[i][j] == 'C') {
-                // Dibujar coleccionable (color azul)
-                mlx_rectangle(mlx, win, x, y, block_size, block_size, 0x0000FF);
+                // Dibujar coleccionable con textura
+                mlx_put_image_to_window(mlx, win, texture->img, x, y);
             } else if (map->map[i][j] == 'E') {
-                // Dibujar salida (color amarillo)
-                mlx_rectangle(mlx, win, x, y, block_size, block_size, 0xFFFF00);
+                // Dibujar salida con textura
+                mlx_put_image_to_window(mlx, win, texture->img, x, y);
             }
             // No dibujar nada para espacios vacíos ('0')
         }
     }
 }
 
+Texture load_texture(void *mlx, char *file_path) {
+    Texture texture;
+
+    // Cargar la textura desde el archivo .xpm
+    texture.img = mlx_xpm_file_to_image(mlx, file_path, &texture.width, &texture.height);
+
+    if (!texture.img) {
+        printf("Error al cargar la textura desde %s\n", file_path);
+    }
+    return texture;
+}
 
 
 
@@ -82,12 +99,16 @@ int main(void) {
     void *mlx;
     void *mlx_win;
     Map game_map;
+	Texture	wall_texture;
 
     mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, MAP_WIDTH * 30, MAP_HEIGHT * 30, "Map Viewer");
+    mlx_win = mlx_new_window(mlx, MAP_WIDTH * 30, MAP_HEIGHT * 30, "so long");
 
     load_map(&game_map);
-    draw_map(mlx, mlx_win, &game_map);
+    wall_texture = load_texture(mlx, "block.xpm");
+
+    draw_map(mlx, mlx_win, &game_map, &wall_texture);
+
 
     mlx_loop(mlx);
 
